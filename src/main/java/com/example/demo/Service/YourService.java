858b901392f2e3fcc269dfb6e74d0835d437a727
kakusan0 <gmakihiro@gmail.com>
2023-11-01 08:00:23 +0900

    aaa

--- src/main/java/com/example/demo/Service/YourService.java
+++ src/main/java/com/example/demo/Service/YourService.java
@@ -2,7 +2,9 @@ package com.example.demo.Service;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,19 +23,24 @@ public class YourService {
         List<YourEntity> entities = yourRepository.findAll();
         FileWriter fileWriter = new FileWriter(filename);
 
-        // CSVヘッダーの書き込み
-        fileWriter.append("Column1, Column2, Column3...\n");
+        // CSVヘッダーの書き込み（Enumを使用）
+        String headerLine = Arrays.stream(CsvHeader.values())
+                                  .map(CsvHeader::getHeader)
+                                  .collect(Collectors.joining(","));
+        fileWriter.append(headerLine).append("\n");
 
         // データの書き込み
         for (YourEntity entity : entities) {
             fileWriter.append(entity.getUpdate_data());
             fileWriter.append(",");
+            // 他のフィールド...
             fileWriter.append("\n");
         }
 
         fileWriter.flush();
         fileWriter.close();
     }
+
     
     @Transactional
     public void updateData2Column() {
@@ -60,4 +67,23 @@ public class YourService {
             throw new IllegalArgumentException("Data does not meet the criteria for modification");
         }
     }
+    
+    public enum CsvHeader {
+        COLUMN1("Column1"),
+        COLUMN2("Column2"),
+        COLUMN3("Column3"),
+        COLUMN4("Column4"),
+        COLUMN5("Column5");
+        
+        private final String header;
+        
+        CsvHeader(String header) {
+            this.header = header;
+        }
+        
+        public String getHeader() {
+            return header;
+        }
+    }
+
 }
