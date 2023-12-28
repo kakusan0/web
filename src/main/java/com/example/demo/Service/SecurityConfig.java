9624ccf728d64a35e74a25a4b3e193d37563ea6f
akihirokakutani <gmakihiro@gmail.com>
2023-12-28 16:37:31 +0900

    修正

--- src/main/java/com/example/demo/Service/SecurityConfig.java
+++ src/main/java/com/example/demo/Service/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.demo.Service;
 
-import com.example.demo.Model.userstorage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
