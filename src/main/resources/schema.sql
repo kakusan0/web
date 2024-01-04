818b63e2e67c4431fc96fc37334784a3075e8a7f
kakusan0 <gmakihiro@gmail.com>
2024-01-04 10:05:14 +0900

    修正

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,25 @@
 CREATE TABLE IF NOT EXISTS userstorage
 (
     id       INT not null AUTO_INCREMENT,
-    mail     VARCHAR(255),
+    email    VARCHAR(255),
     username VARCHAR(255),
-    pw       VARCHAR(255),
+    password VARCHAR(255),
     NG_flag  BOOLEAN,
     NG_date  DATETIME,
     PRIMARY KEY (id),
-    UNIQUE (mail),
+    UNIQUE (email),
     UNIQUE (username)
+);
+
+CREATE TABLE IF NOT EXISTS postmaster
+(
+    NationalLocalGovernmentCode INT not null,
+    oldPostalCode               VARCHAR(255),
+    PostCode                    VARCHAR(255),
+    prefecturesKANA             VARCHAR(255),
+    municipalitiesKANA          VARCHAR(255),
+    TownAreaKANA                VARCHAR(255),
+    prefecturesKANZI            VARCHAR(255),
+    municipalitiesKANZI         VARCHAR(255),
+    TownAreaKANZI               VARCHAR(255)
 );
