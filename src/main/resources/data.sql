757b40750c6e8bd28432feccf3e607e089d91864
kakusan0 <gmakihiro@gmail.com>
2024-01-08 11:40:08 +0900

    修正

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT
-INTO postmaster(LeftPostCode, RightPostCode)
-SELECT IF(LENGTH(a.PostCode) < 6, LEFT(a.PostCode, 4), LEFT(a.PostCode, 3))
-     , IF(LENGTH(a.PostCode) > 6, RIGHT(a.PostCode, 4), RIGHT(a.PostCode, 3))
-FROM temppostmaster a;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,18 +1,40 @@
-CREATE TABLE IF NOT EXISTS userstorage
+/*
+テーブル削除
+*/
+drop table IF EXISTS oldpostmaster;
+
+drop view IF EXISTS postmaster;
+
+drop table IF EXISTS prefecture;
+
+drop table IF EXISTS userstorage;
+
+/*
+
+*/
+CREATE TABLE IF NOT EXISTS oldpostmaster
 (
-    id       INT not null AUTO_INCREMENT,
-    email    VARCHAR(255),
-    username VARCHAR(255),
-    password VARCHAR(255),
-    NG_flag  BOOLEAN,
-    NG_date  DATETIME,
-    PRIMARY KEY (id),
-    UNIQUE (email),
-    UNIQUE (username)
+    PostCode            VARCHAR(255),
+    prefecturesKANA     VARCHAR(255),
+    municipalitiesKANA  VARCHAR(255),
+    TownAreaKANA        VARCHAR(255),
+    prefecturesKANZI    VARCHAR(255),
+    municipalitiesKANZI VARCHAR(255),
+    TownAreaKANZI       VARCHAR(255)
 );
+SET PERSIST local_infile= 1;
+/*
+utf_ken_all.csv　ファイルインポート
+*/
+LOAD DATA local INFILE 'C:/Users/gmaki/Documents/web/src/main/resources/utf_ken_all.csv'
+    INTO table oldpostmaster FIELDS TERMINATED BY
+        ',' ENCLOSED BY
+        '"' LINES TERMINATED BY
+        '\n' IGNORE 1 LINES;
 
-drop view postmaster;
-
+/*
+ビュー作成
+*/
 create view postmaster
             (
              LeftPostCode, RightPostCode, prefecturesKANZI, municipalitiesKANZI, TownAreaKANZI
@@ -27,19 +49,44 @@ select SUBSTRING(a.PostCode, 1, 3)
      , a.prefecturesKANZI
      , a.municipalitiesKANZI
      , a.TownAreaKANZI
-from temppostmaster a;
+from oldpostmaster a;
 
-TRUNCATE TABLE temppostmaster;
-
-drop table temppostmaster;
-
-CREATE TABLE IF NOT EXISTS temppostmaster
+/*
+都道府県テーブル
+*/
+CREATE TABLE IF NOT EXISTS prefecture
 (
-    PostCode            VARCHAR(255),
-    prefecturesKANA     VARCHAR(255),
-    municipalitiesKANA  VARCHAR(255),
-    TownAreaKANA        VARCHAR(255),
-    prefecturesKANZI    VARCHAR(255),
-    municipalitiesKANZI VARCHAR(255),
-    TownAreaKANZI       VARCHAR(255)
+    prefectureCode int AUTO_INCREMENT,
+    prefectures    VARCHAR(255),
+    PRIMARY KEY (prefectureCode)
+);
+
+insert
+into prefecture(prefectures)
+select DISTINCT b.prefecturesKANZI
+from oldpostmaster b;
+
+/*
+都道府県コードカラム追加　oldpostmasterテーブルに
+*/
+alter table oldpostmaster
+    add column prefectureCode int first;
+
+update oldpostmaster c
+    left join prefecture d
+    on c.prefecturesKANZI = d.prefectures
+set c.prefectureCode = d.prefectureCode
+where c.prefecturesKANZI = d.prefectures;
+
+CREATE TABLE `userstorage`
+(
+    `id`       int NOT NULL AUTO_INCREMENT,
+    `email`    varchar(255) DEFAULT NULL,
+    `username` varchar(255) DEFAULT NULL,
+    `password` varchar(255) DEFAULT NULL,
+    `NG_flag`  tinyint(1)   DEFAULT NULL,
+    `NG_date`  datetime     DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `email` (`email`),
+    UNIQUE KEY `username` (`username`)
 );
